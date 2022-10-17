be3c88d68e5daf556bb957ac406f111497b891a9
1E10 <sample@bths.edu>
2022-10-17 14:27:05 -0400

    10/17/22

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,9 +1,9 @@
 public class LinearEquation {
     //instance variables
-    private int x1;
-    private int y1;
-    private int x2;
-    private int y2;
+    final private int  x1;
+    final private int y1;
+    final private int x2;
+    final private int y2;
 
     //Linear Equation constructor
     public LinearEquation(int x1, int y1, int x2, int y2) {
@@ -11,6 +11,32 @@ public class LinearEquation {
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
+    }
+
+/* Calculates and returns distance between (x1, y1) and (x2, y2), rounded to
+   the nearest hundredth */
+    public double distance() {
+        double distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1) , 2));
+        distance = Math.round(distance * 100.0) / 100.0;
+        return distance;
+    }
+
+    /* Calculates and returns the slope of the line between (x1, y1) and
+   (x2, y2), rounded to the nearest hundredth */
+    public double slope() {
+        double differenceY = y2 - y1;
+        double differenceX = x2 - x1;
+        double slope = (differenceY) / (differenceX);
+        slope = Math.round(slope * 100.0) / 100.0;
+        return slope;
+    }
+
+    /* Calculates and returns the y-intercept of the line between (x1, y1) and
+   (x2, y2), rounded to the nearest hundredth */
+    public double yIntercept() {
+
+        return 0.0;
+
     }
 
 
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -4,10 +4,12 @@ public class LinearEquationRunner {
     public static void main (String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        //user creates an object
+        //program welcomes user and asks user to input coordinates
         System.out.println("Welcome!");
         System.out.println("Enter coordinate 1: ");
         String coordinate1 = scan.nextLine();
+        System.out.println("Enter coordinate 2:" );
+        String coordinate2 = scan.nextLine();
 
         //index of parentheses and commas
         int indexOfParentheses = coordinate1.indexOf(")");
@@ -17,14 +19,20 @@ public class LinearEquationRunner {
         int x1 = Integer.parseInt(coordinate1.substring(1,indexOfComma));
         int y1 = Integer.parseInt(coordinate1.substring(indexOfComma + 2, indexOfParentheses));
 
-        //parsing of second coordinate
-        System.out.println("Enter coordinate 2:" );
-        String coordinate2 = scan.nextLine();
-        int x2 = Integer.parseInt(coordinate2.substring(1,indexOfComma));
-        int y2 = Integer.parseInt(coordinate2.substring(indexOfComma + 2, indexOfParentheses));
+        //index of parentheses and commas
+        int indexOfParentheses2 = coordinate2.indexOf(")");
+        int indexOfComma2 = coordinate2.indexOf(", ");
 
-        //prints coordinates
-        System.out.println("The two points are: " + "(" + x1 + ", " + y1 + ")");
+        //parsing of second coordinate
+        int x2 = Integer.parseInt(coordinate2.substring(1,indexOfComma2));
+        int y2 = Integer.parseInt(coordinate2.substring(indexOfComma2 + 2, indexOfParentheses2));
+
+        //creates coordinate object
+        LinearEquation equation = new LinearEquation(x1, y1, x2, y2);
+        // System.out.println("The two points are: " + "(" + x1 + ", " + y1 + ")" + " and " + "(" + x2 + ", " + y2 + ")");
+
+        System.out.println(equation.distance());
+        System.out.println(equation.slope());
 
 
 
